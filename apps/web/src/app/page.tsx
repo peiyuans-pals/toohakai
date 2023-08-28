9b9192087a537d5885e9dda6f4a43139cee0059d
Zen <hteh002@e.ntu.edu.sg>
2023-08-28 15:06:18 +0800

    New branch and Chakra UI installation

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { log } from "logger";
 import { CounterButton, NewTabLink } from "ui";
-
+import { Spinner } from '@chakra-ui/react'
+import * as React from 'react';
 export const metadata = {
   title: "Store | Kitchen Sink",
 };
@@ -13,7 +14,9 @@ export default function Store() {
         Store <br />
         <span>Kitchen Sink</span>
       </h1>
+      <Spinner />
       <CounterButton />
+      
       <p className="description">
         Built With{" "}
         <NewTabLink href="https://turbo.build/repo">Turborepo</NewTabLink> +{" "}
