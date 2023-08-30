804c1d22a48ac4854de6951900ed4da9568ffa90
zen <hteh002@e.ntu.edu.sg>
2023-08-30 08:35:50 +0800

    New drawer added for creating/editing question bank

--- apps/web/src/app/Teacher/question_bank/page.tsx
+++ apps/web/src/app/Teacher/question_bank/page.tsx
@@ -1,24 +1,86 @@
 "use client";
-import { CounterButton } from "ui";
 import Sidebar from "../../Components/Sidebar";
 import * as React from "react";
-import { Select, Box, Flex, Button } from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
+import { Select, Box, Flex, Button, Stack, Input } from "@chakra-ui/react";
 import {
   Table,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
   Td,
-  TableCaption,
   TableContainer,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
+
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
 } from "@chakra-ui/react";
 
 export default function QuestionBank() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
   return (
     <>
       <Sidebar />
+      <Drawer
+        size="md"
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create New Question</DrawerHeader>
+
+          <DrawerBody>
+            <Stack>
+            <FormControl isRequired>
+              <FormLabel>Question</FormLabel>
+              <Input type="text" />
+              <FormHelperText>Eg: What is the square root of 256?</FormHelperText>
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Topic</FormLabel>
+              <Input type="text" />
+              <FormHelperText>Eg: Discrete Mathematics</FormHelperText>
+            </FormControl>
+            <FormControl isRequired marginTop={12}>
+              <FormLabel>Option 1</FormLabel>
+              <Input type="text" />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Option 2</FormLabel>
+              <Input type="text" />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Option 3</FormLabel>
+              <Input type="text" />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Option 4</FormLabel>
+              <Input type="text" />
+            </FormControl>
+            </Stack>
+          </DrawerBody>
+
+          <DrawerFooter>
+              <Button colorScheme="blue" type="submit">Create New Question</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
       <Box>
         <Flex
           flexDirection="column"
@@ -26,24 +88,22 @@ export default function QuestionBank() {
           paddingLeft={75}
           paddingRight={75}
         >
-          <h1 className="title">
-            Question Bank Page
-          </h1>
-          <Select 
-          placeholder="-No Filter Selected-"
-          marginTop={5}>
+          <h1 className="title">Question Bank Page</h1>
+          <Select placeholder="-No Filter Selected-" marginTop={5}>
             <option value="option1">Discrete Maths</option>
-
           </Select>
-          <Button 
-          marginTop={5}
-          marginRight={5}
-          alignSelf="flex-end" 
-          colorScheme="blue" 
-          size="sm"
-          >New</Button>
-          <TableContainer
-          marginTop={5}>
+          <Button
+            ref={btnRef}
+            onClick={onOpen}
+            marginTop={5}
+            marginRight={5}
+            alignSelf="flex-end"
+            colorScheme="blue"
+            size="sm"
+          >
+            New
+          </Button>
+          <TableContainer marginTop={5}>
             <Table variant="simple" size="sm">
               <Thead>
                 <Tr>
@@ -56,22 +116,45 @@ export default function QuestionBank() {
                 <Tr>
                   <Td>Discrete Maths</Td>
                   <Td>If a set B has n elements, then what is... </Td>
-                  <Td><Button colorScheme="blue" size="sm">Edit</Button></Td>
-                  <Td><Button colorScheme="red" size="sm">Delete</Button></Td>
-
+                  <Td>
+                    <Button colorScheme="blue" size="sm">
+                      Edit
+                    </Button>
+                  </Td>
+                  <Td>
+                    <Button colorScheme="red" size="sm">
+                      Delete
+                    </Button>
+                  </Td>
                 </Tr>
                 <Tr>
                   <Td>Discrete Maths</Td>
                   <Td>If X and y are the two finite sets, such that...</Td>
-                  
-                  <Td><Button colorScheme="blue" size="sm">Edit</Button></Td>
-                  <Td><Button colorScheme="red" size="sm">Delete</Button></Td>
+
+                  <Td>
+                    <Button colorScheme="blue" size="sm">
+                      Edit
+                    </Button>
+                  </Td>
+                  <Td>
+                    <Button colorScheme="red" size="sm">
+                      Delete
+                    </Button>
+                  </Td>
                 </Tr>
                 <Tr>
                   <Td>Discrete Maths</Td>
                   <Td>Find the missing number in the sequence: 5, 10...</Td>
-                  <Td><Button colorScheme="blue" size="sm">Edit</Button></Td>
-                  <Td><Button colorScheme="red" size="sm">Delete</Button></Td>
+                  <Td>
+                    <Button colorScheme="blue" size="sm">
+                      Edit
+                    </Button>
+                  </Td>
+                  <Td>
+                    <Button colorScheme="red" size="sm">
+                      Delete
+                    </Button>
+                  </Td>
                 </Tr>
               </Tbody>
             </Table>
