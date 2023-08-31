949136938f6dfe50b4499b68358a91c924ad53d3
Alan Seng <56150925+iAlanx3@users.noreply.github.com>
2023-08-31 20:42:35 +0800

    Frontend/QuizReport
    - Having issue with padding in between topic and date picker
    - Date picker placeholder not showing properly
    - QuizReport_Table Component having issue displaying the items
    - TODO: Open a new window with quiz

---
+++ apps/web/src/app/Components/QuizReport_Table.tsx
@@ -0,0 +1,33 @@
+import { Table,Thead,Tbody,Tr,Th,Td,TableContainer, Button, } from "@chakra-ui/react"
+
+
+export default function QuizReport_Table({quizreport_summary}) {
+    return(
+        <>
+            <TableContainer>
+            <Table variant="simple" size="sm">
+              <Thead>
+                <Tr>
+                  <Th>Topic</Th>
+                  <Th>Date</Th>
+                  <Th>Options</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {
+                  //map json file
+                  quizreport_summary["quiz report"].map((record) => 
+                  {
+                    <>
+                    <Td>{record["quiz topic"]}</Td>
+                    <Td>{record.date}</Td>
+                    <Td><Button>Expand</Button></Td>
+                    </>
+                  })
+                }
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </>
+    )
+}
