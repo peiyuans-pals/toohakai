99421d2285f0c2c388aaa57ba22b47756e020897
Alan Seng <56150925+iAlanx3@users.noreply.github.com>
2023-08-31 20:42:35 +0800

    Frontend/QuizReport
    - Having issue with padding in between topic and date picker
    - Date picker placeholder not showing properly
    - QuizReport_Table Component having issue displaying the items
    - TODO: Open a new window with quiz

--- apps/web/src/app/Teacher/quiz_report/page.tsx
+++ apps/web/src/app/Teacher/quiz_report/page.tsx
@@ -2,17 +2,49 @@
 import { CounterButton } from "ui";
 import Sidebar from "../../Components/Sidebar";
 import * as React from "react";
+import { Box, Button, Flex, Input, Select, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import dummyData from "../../MockData/quiz_report.json" assert {type: 'json'};
+import QuizReport_Table from "../../Components/QuizReport_Table";
 
 export default function Page() {
+
   return (
     <>
       <Sidebar/>
-      <div className="container">
-      <h1 className="title">
-        Quiz Report Page <br />
-        <span>Content here</span>
-      </h1>
-    </div>
+      <Box>
+        <Flex
+          flexDirection = "column"
+          paddingTop={100}
+          paddingLeft={75}
+          paddingRight={75}
+        >
+          <h1 className="title">Quiz Report Page</h1>
+          <Flex
+            className="selection column"
+            flexDirection="row"
+          >
+            <Select 
+              placeholder='Select Quiz Topic' 
+              width={(parent.innerWidth)*(2/3)}
+            >
+              <option value='option1'>Discrete Math</option>              
+            </Select>
+            <Input
+              paddingLeft={50}
+              placeholder="Select Month and Year"
+              type="month"
+            />
+          </Flex>
+          <QuizReport_Table quizreport_summary={dummyData}/>
+        </Flex>
+      </Box>
     </>
   );
   }
+
+/**
+ * <h1 className="title">
+        Quiz Report Page <br />
+        <span>Content here</span>
+      </h1>
+ * **/
