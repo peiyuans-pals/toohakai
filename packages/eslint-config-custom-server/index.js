39fad172daf2ea5bf4ff7e518f1d1d58fe827d49
dyllon <dyllon001@e.ntu.edu.sg>
2023-09-09 00:56:24 +0800

    add typescript-eslint

--- packages/eslint-config-custom-server/index.js
+++ packages/eslint-config-custom-server/index.js
@@ -1,13 +1,15 @@
 module.exports = {
-  extends: ["eslint:recommended", "turbo"],
+  extends: ["eslint:recommended","plugin:@typescript-eslint/recommended", "turbo"],
   env: {
     node: true,
     es6: true,
   },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
   //   ecmaVersion: "latest",
     sourceType: "module",
   },
+  plugins: ["@typescript-eslint"],
   overrides: [
     {
       files: ["**/__tests__/**/*"],
--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,4 +1,4 @@
-export const log = (str: any) => {
+export const log = (str: string) => {
   // console.log("\x1b[33m" + "log" + "\x1b[0m" + ": " + str);
   console.log(str)
 };
