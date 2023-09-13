404e89d2af550fa1dec59b0b404e5b016c7dccd7
dyllon <dyllon001@e.ntu.edu.sg>
2023-09-13 23:57:46 +0800

    fix trpc api url

--- apps/web/src/utils/trpc/server.ts
+++ apps/web/src/utils/trpc/server.ts
@@ -2,25 +2,7 @@ import { type AppRouter } from "api";
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import superjson from "superjson";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-
-function getBaseUrl() {
-  // if (typeof window !== 'undefined')
-  //   // browser should use relative path
-  //   return '';
-  // if (process.env.VERCEL_URL)
-  //   // reference for vercel.com
-  //   return `https://${process.env.VERCEL_URL}`;
-  if (process.env.NEXT_PUBLIC_TOOHAKAI_API_URL)
-    // reference for render.com
-    return process.env.NEXT_PUBLIC_TOOHAKAI_API_URL;
-  if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test")
-    // assume localhost
-    // return `http://localhost:${String(Stringprocess.env.PORT ?? 5001)}`;
-    return `http://localhost:5001`;
-  throw new Error(
-    "No NEXT_PUBLIC_TOOHAKAI_API_URL environment variable specified."
-  );
-}
+import { getBaseUrl } from "./lib";
 
 export const trpcServer = (cookies: any) =>
   createTRPCProxyClient<AppRouter>({
