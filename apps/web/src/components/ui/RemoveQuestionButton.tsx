778ce5b41d6fb11a23ff187bebe736fb606ccc76
zen <hteh002@e.ntu.edu.sg>
2023-09-14 12:02:16 +0800

    Prettify

--- apps/web/src/components/ui/RemoveQuestionButton.tsx
+++ apps/web/src/components/ui/RemoveQuestionButton.tsx
@@ -24,16 +24,16 @@ export const RemoveQuestionButton = ({ initialData }: Props) => {
   const questionBanksData = initialData;
   return (
     <DropdownMenu>
-        <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger asChild>
         <Button variant="destructive">Remove</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>This action cannot be undone</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 w-full">
-            Confirm Removal
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>This action cannot be undone</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 w-full">
+          Confirm Removal
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
