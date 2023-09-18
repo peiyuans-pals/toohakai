938045a4637343f8e98e9a280e6e3b89b95fee3c
dyllon <dyllon001@e.ntu.edu.sg>
2023-09-19 00:43:42 +0800

    add next.js headers

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,5 +6,55 @@ module.exports = {
   experimental: {
     outputFileTracingRoot: path.join(__dirname, "../../"),
     serverActions: true
-  }
+  },
+  async headers() {
+    const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+              style-src 'self' 'nonce-${nonce}';
+              img-src 'self' blob: data:;
+              font-src 'self';
+              object-src 'none';
+              base-uri 'self';
+              form-action 'self';
+              frame-ancestors 'none';
+              block-all-mixed-content;
+              upgrade-insecure-requests;
+            `
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=()',
+          }
+        ],
+      },
+    ]
+  },
 };
