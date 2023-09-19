db5de0ce66479bb558b4d20af4307de399682556
Zen <hteh002@e.ntu.edu.sg>
2023-09-20 07:56:41 +0800

    Update page.tsx

--- apps/web/src/app/student/quiz/page.tsx
+++ apps/web/src/app/student/quiz/page.tsx
@@ -1,3 +1,4 @@
+"use server"
 import { cookies } from "next/headers";
 import { ButtonGroup, ButtonGroupItem } from "src/components/ui/ButtonGroup";
 import { trpcServer } from "../../../utils/trpc/server";
