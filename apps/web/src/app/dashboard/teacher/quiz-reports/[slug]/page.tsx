d46f6fce10bd974fc3c54850cb707ff80cda372b
Alan Seng <56150925+iAlanx3@users.noreply.github.com>
2023-09-22 00:48:03 +0800

    Prettier-fy

--- apps/web/src/app/dashboard/teacher/quiz-reports/[slug]/page.tsx
+++ apps/web/src/app/dashboard/teacher/quiz-reports/[slug]/page.tsx
@@ -1,40 +1,55 @@
-"use client"
+"use client";
 import { DashboardView, Heading } from "../../../../../components/ui";
 import React, { Dispatch, SetStateAction, useState } from "react";
 import initialData from "../../../../../mockdata/teacher_quizreports_expanded.json";
-import { StudentDropDownBar } from "./StudentDropDownBar";
-import { QuizResultTable } from "./QuizResultTable";
+import { StudentDropDownBar } from "./_components/StudentDropDownBar";
+import { QuizResultTable } from "./_components/QuizResultTable";
 
 interface Props {
   quizreportData: Record<string, any>[];
   setStudentScore: Dispatch<SetStateAction<number>>;
 }
 
-export default function ExpandedReport({ params }: { params: { slug: string }}) {
+export default function ExpandedReport({
+  params
+}: {
+  params: { slug: string };
+}) {
   //trpc.call for quizreportexpanded data window.location.href.split("/").pop()
   const [pageURL, setPageURL] = useState(params.slug);
-  const [studentquiz, setStudentQuiz] = useState(initialData.data[0].quiz)
+  const [studentquiz, setStudentQuiz] = useState(initialData.data[0].quiz);
 
   var student_names = initialData.data.map((student) => student.student);
   const [studentName, setStudentName] = useState(student_names[0]);
-  
+
   let studentscore = initialData.data[0].score;
-  let filteredStudentQuiz =initialData.data[0].quiz;
+  let filteredStudentQuiz = initialData.data[0].quiz;
   const fullscore = initialData.fullscore;
-  
+
   if (studentName) {
-    var filteredStudent = initialData.data.find((student) => student.student===studentName);
+    var filteredStudent = initialData.data.find(
+      (student) => student.student === studentName
+    );
     filteredStudentQuiz = filteredStudent?.quiz!;
-    studentscore = filteredStudent?.score!    
+    studentscore = filteredStudent?.score!;
   }
 
   return (
     <DashboardView>
       <Heading>{initialData.name}</Heading>
       <div className="flex pt-2">
-        <StudentDropDownBar studentlist={student_names} student={studentName} setStudent={setStudentName}/>
+        <StudentDropDownBar
+          studentlist={student_names}
+          student={studentName}
+          setStudent={setStudentName}
+        />
       </div>
-      <QuizResultTable studentquizResult={filteredStudentQuiz} studentname={studentName} studentscore={studentscore} fullscore={fullscore}/>
+      <QuizResultTable
+        studentquizResult={filteredStudentQuiz}
+        studentname={studentName}
+        studentscore={studentscore}
+        fullscore={fullscore}
+      />
     </DashboardView>
   );
 }
