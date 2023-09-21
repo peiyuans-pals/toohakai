465ed8241d594a22937e7f38fa7f6e86a48d698e
Alan Seng <56150925+iAlanx3@users.noreply.github.com>
2023-09-22 00:48:03 +0800

    Update

--- apps/web/src/app/dashboard/teacher/quiz-reports/[slug]/_components/StudentDropDownBar.tsx
+++ apps/web/src/app/dashboard/teacher/quiz-reports/[slug]/_components/StudentDropDownBar.tsx
@@ -1,26 +1,39 @@
 "use client";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { Dispatch, SetStateAction, useState } from "react";
 
 interface Props {
-    studentlist: string[];
-    student: string;
-    setStudent: Dispatch<SetStateAction<string>>;
-  }
-
-export const StudentDropDownBar = ({student, studentlist, setStudent}: Props) => {
-    return(
-        <DropdownMenu>
-                    <DropdownMenuTrigger><Button>{student}</Button></DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        {studentlist.map((item) => (
-                            <DropdownMenuItem onSelect={() => setStudent(item)}>{item}</DropdownMenuItem>
-                        ))}
-                    </DropdownMenuContent>
-            </DropdownMenu>
-    );
+  studentlist: string[];
+  student: string;
+  setStudent: Dispatch<SetStateAction<string>>;
 }
+
+export const StudentDropDownBar = ({
+  student,
+  studentlist,
+  setStudent
+}: Props) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button>{student}</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {studentlist.map((item) => (
+          <DropdownMenuItem onSelect={() => setStudent(item)}>
+            {item}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 /**
  * {quizreport_topics.teacher_quizreports_topics.map((item) => (
                             <DropdownMenuItem onSelect={() => setTopic(item.topic)}>{item.topic}</DropdownMenuItem>
--- apps/web/src/app/dashboard/teacher/quiz-reports/_components/Cards.tsx
+++ apps/web/src/app/dashboard/teacher/quiz-reports/_components/Cards.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
 import { SizeIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
@@ -11,10 +17,16 @@ export const QuizReportCards = ({ initialData }: Props) => {
   return (
     <div className="grid grid-cols-4 gap-4">
       {initialData.map((item) => (
-          <Link target={"_blank"} key={item.id} href={`/dashboard/teacher/quiz-reports/${item.id}`}>
-            <Card className=" h-[200px]">
+        <Link
+          target={"_blank"}
+          key={item.id}
+          href={`/dashboard/teacher/quiz-reports/${item.id}`}
+        >
+          <Card className=" h-[200px]">
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">{item.topic}</CardTitle>
+              <CardTitle className="text-sm font-medium">
+                {item.topic}
+              </CardTitle>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -32,21 +44,24 @@ export const QuizReportCards = ({ initialData }: Props) => {
               <div className="text-2xl font-bold">{item.name}</div>
             </CardContent>
             <CardFooter className="flex-col justify-start items-start">
-              <p className="text-xs text-muted-foreground">
-                {item.length} Questions
-              </p>
-              <p className="text-xs text-muted-foreground">
-                {item.date}
-              </p>
+              <div className=" flex">
+                <p className="text-xs text-muted-foreground">
+                  {item.length} Questions
+                </p>
+                <div className=" ml-auto mr-0">
+                  <b>
+                    {item.average}/{item.fullscore}
+                  </b>
+                </div>
+              </div>
+              <p className="text-xs text-muted-foreground">{item.date}</p>
             </CardFooter>
           </Card>
-          </Link>
+        </Link>
       ))}
     </div>
   );
 };
-
-
 
 /*
     <Button variant="outline" size="icon" 
