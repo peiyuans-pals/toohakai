197390a89b67e8968b67e61a0ef180ab7c3ea52f
Ryan Khong <wykhongryan@gmail.com>
2023-09-21 14:25:03 +0800

    init cypress branch

--- apps/web/cypress/e2e/app.cy.ts
+++ apps/web/cypress/e2e/app.cy.ts
@@ -1,11 +1,12 @@
 describe("Navigation", () => {
   it("should navigate to the about page", () => {
     // Start from the index page
-    cy.visit("http://localhost:3002/");
+    cy.visit("https://www.toohakai.fun");
 
     // The page should contain an h1 with "Toohakai" and some other text
     cy.get("h1").contains("Toohakai");
     cy.get("p").contains("A really cool quiz app");
+    cy.get('[data-cy="login-button"]').contains("Login");
 
     // cy.get('[data-cy="login-button"]').click()
 
