171c0f11819cbe18b87e30fdbb4360f47e8e16ae
zen <hteh002@e.ntu.edu.sg>
2023-09-22 23:48:12 +0800

    Quiz View logic

--- apps/web/src/app/teacher/quiz/_components/QuizView.tsx
+++ apps/web/src/app/teacher/quiz/_components/QuizView.tsx
@@ -6,9 +6,10 @@ import {
 } from "src/app/teacher/quiz/_components/ButtonGrid";
 import { trpc } from "../../../../utils/trpc/client";
 import { TrpcReactQueryOptions } from "../../../../utils/trpc/lib";
-import { CheckCircledIcon } from "@radix-ui/react-icons";
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from "react";
 import { Progress } from "@/components/ui/progress";
+import { Button } from "@/components/ui/button";
 
 interface Props {
   id: number;
@@ -18,10 +19,12 @@ export const QuizView = ({ id, initialData }: Props) => {
   const { data: questionBank, refetch } = trpc.questionBank.get.useQuery(id, {
     initialData
   });
-  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+  const router = useRouter()
   const [countdown, setCountdown] = useState<number>(10);
   const [timerEnded, setTimerState] = useState<boolean>(false);
-  const percentage = [25,10,15,50]
+  const [questionIndex, setQuestionIndex] = useState<number>(0);
+  
+  const percentage = [25, 10, 15, 50]; //Percentage of students choosing option 1,2,3,4 respectively
 
   // every second, decrement countdown
   useEffect(() => {
@@ -29,12 +32,25 @@ export const QuizView = ({ id, initialData }: Props) => {
       setTimeout(() => setCountdown(countdown - 1), 1000);
     } else {
       //setCountdown(10);
-      setTimerState(true)
+      setTimerState(true);
     }
   }, [countdown]);
-  const question_id = 0; //mock question ID
-  
- 
+  const question_id = [0, 1]; //mock question IDs
+  let i = 0;
+
+  function nextQn() {
+    
+    if (questionIndex < question_id.length-1){
+    setCountdown(10);
+    setTimerState(false);
+    setQuestionIndex(questionIndex + 1);
+    return 
+    }
+    router.push("/dashboard/")
+    
+
+  }
+
   if (!questionBank) {
     return (
       <div className="flex items-center justify-center h-screen">
@@ -46,13 +62,14 @@ export const QuizView = ({ id, initialData }: Props) => {
   return (
     <div className="p-5 flex flex-col h-screen">
       <h1 className="text-4xl font-bold text-gray-900">{questionBank.title}</h1>
-      <p className="text-xl">{questionBank.questions[question_id].title}</p>
+      <p className="text-xl">
+        {questionBank.questions[question_id[questionIndex]].title}
+      </p>
       <Progress className="mt-5" value={countdown * 10}></Progress>
-        <div
-          className="flex flex-col mt-auto mb-10"
-        >
-          <ButtonGrid>
-            {questionBank.questions[question_id].answers.map((answer, key) => (
+      <div className="flex flex-col mt-auto mb-10">
+        <ButtonGrid>
+          {questionBank.questions[question_id[questionIndex]].answers.map(
+            (answer, key) => (
               <div key={key} className="flex items-center justify-center">
                 <ButtonGridItem
                   percentage={percentage[key]}
@@ -63,17 +80,13 @@ export const QuizView = ({ id, initialData }: Props) => {
                   {answer.text}
                 </ButtonGridItem>
               </div>
-            ))}
-          </ButtonGrid>
-
-          {/* <Button
-            disabled={isSubmitted}
-            type="submit"
-            className=" text-xl mt-2"
-          >
-            Submit
-          </Button> */}
-        </div>
+            )
+          )}
+        </ButtonGrid>
+        <Button className="text-4xl h-20" onClick={nextQn} disabled={!timerEnded}>
+          Next
+        </Button>
+      </div>
     </div>
   );
 };
--- apps/web/src/app/teacher/quiz/page.tsx
+++ apps/web/src/app/teacher/quiz/page.tsx
@@ -3,12 +3,18 @@ import { cookies } from "next/headers";
 import { ButtonGroup, ButtonGroupItem } from "src/app/student/quiz/_components/ButtonGroup";
 import { trpcServer } from "../../../utils/trpc/server";
 import { QuizView } from "./_components/QuizView";
+import { Button } from "@/components/ui/button";
 
 export default async function Quiz() {
+  function onSubmit(){
+    console.log("Click")
+  }
   const id = 1;
   const questionBank = await trpcServer(cookies).questionBank.get.query(id);
   return (
-  <QuizView id={1} initialData={questionBank}></QuizView>
+    <div>
+  <QuizView id={1} key={1} initialData={questionBank}></QuizView>
+  </div>
   
   );
 }
