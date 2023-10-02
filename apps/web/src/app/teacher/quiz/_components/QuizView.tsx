8e4c35b42884f19426e23905aebfcd880444cf36
Zen <zteh2461@gmail.com>
2023-10-02 08:41:58 +0800

    prettier

--- apps/web/src/app/teacher/quiz/_components/QuizView.tsx
+++ apps/web/src/app/teacher/quiz/_components/QuizView.tsx
@@ -43,7 +43,7 @@ export const QuizView = ({ id, initialData }: Props) => {
       return;
     }
     if (countdown > 0 && questionEndedState && !manualControl) {
-      setTimeout(() => setCountdown(countdown - 1), 1000);      
+      setTimeout(() => setCountdown(countdown - 1), 1000);
       return;
     }
     setQuestionEndedState(true);
