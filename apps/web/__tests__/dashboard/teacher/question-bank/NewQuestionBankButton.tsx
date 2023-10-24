5e4ff7b920f436725986949e3b7fae88502cfea1
Zen <zteh2461@gmail.com>
2023-10-24 21:15:57 +0800

    Added some testing code

---
+++ apps/web/__tests__/dashboard/teacher/question-bank/NewQuestionBankButton.tsx
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import { NewQuestionBankButton } from "@/app/dashboard/teacher/question-banks/_components/NewQuestionBankButton" 
+
+describe("NewQuestionBankButton", () => {
+  it("should render New Question Bank Button correctly", () => {
+    render(<NewQuestionBankButton />);
+
+    const newQuestionBankButtonText = screen.getByText(/Create New Question Bank/i);
+
+    expect(newQuestionBankButtonText).toBeInTheDocument();
+  });
+});
