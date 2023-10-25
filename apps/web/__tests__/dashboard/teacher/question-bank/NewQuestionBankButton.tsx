7c74bd0110cfd8cc9b2de8a4d13db53bd2642d34
Zen <zteh2461@gmail.com>
2023-10-25 11:34:20 +0800

    prettier

--- apps/web/__tests__/dashboard/teacher/question-bank/NewQuestionBankButton.tsx
+++ apps/web/__tests__/dashboard/teacher/question-bank/NewQuestionBankButton.tsx
@@ -2,13 +2,15 @@
  * @jest-environment jsdom
  */
 import { render, screen } from "@testing-library/react";
-import { NewQuestionBankButton } from "@/app/dashboard/teacher/question-banks/_components/NewQuestionBankButton" 
+import { NewQuestionBankButton } from "@/app/dashboard/teacher/question-banks/_components/NewQuestionBankButton";
 
 describe("NewQuestionBankButton", () => {
   it("should render New Question Bank Button correctly", () => {
     render(<NewQuestionBankButton />);
 
-    const newQuestionBankButtonText = screen.getByText(/Create New Question Bank/i);
+    const newQuestionBankButtonText = screen.getByText(
+      /Create New Question Bank/i
+    );
 
     expect(newQuestionBankButtonText).toBeInTheDocument();
   });
