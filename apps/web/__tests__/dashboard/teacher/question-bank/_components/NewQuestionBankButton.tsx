6cfec4e01b1b79f7ab061f59637ef51c788a6d60
Zen <hteh002@e.ntu.edu.sg>
2023-10-26 09:05:27 +0800

    fixed tsconfig for jest

--- apps/web/__tests__/dashboard/teacher/question-bank/_components/NewQuestionBankButton.tsx
+++ apps/web/__tests__/dashboard/teacher/question-bank/_components/NewQuestionBankButton.tsx
@@ -2,64 +2,16 @@
  * @jest-environment jsdom
  */
 import "@testing-library/jest-dom";
-import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure
-} from "../../../../../../../packages/api/src/utils/trpc";
 
 import * as React from "react";
-import { z } from "zod";
-import { prisma } from "../../../../../../../packages/api/src/utils/prisma";
-import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent, cleanup } from "@testing-library/react";
 import { NewQuestionBankButton } from "./../../../../../src/app/dashboard/teacher/question-banks/_components/NewQuestionBankButton";
-import { trpc } from "../../../../../src/utils/trpc/client";
-
-jest.mock("../../../../../src/utils/trpc/client", () => {
-  return {
-    trpc: {
-      useContext: () => ({
-        questionBank: {
-          create: protectedProcedure
-            .meta({
-              description: "Create a questionBank!"
-            })
-            .input(
-              z.object({
-                title: z.string().min(4).max(32)
-              })
-            )
-            .mutation(async (opts) => {
-              console.log("trying to create a questionBank");
-
-              const questionBank = await prisma.questionBank.create({
-                data: {
-                  title: opts.input.title,
-                  // authorId: opts.ctx.user.data.user!.id,
-                  author: {
-                    connect: {
-                      id: opts.ctx.user.data.user!.id
-                    }
-                  }
-                },
-                include: {
-                  author: true
-                }
-              });
-
-              return {
-                success: true,
-                status: "questionBank created successfully",
-                questionBank
-              };
-            })
-        }
-      })
-    }
-  };
-});
+import { SessionContextProvider }
 
 describe("NewQuestionBankButton", () => {
+	afterEach(()=>{
+		cleanup();
+	})
   it("renders the button and opens the dialog on click", async () => {
     render(<NewQuestionBankButton />);
 
